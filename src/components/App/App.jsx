2da0f759904714581d716240c65681b53571f019
Sayvon <sedmon033@gmail.com>
2024-05-04 13:44:57 -0500

    add SideNav

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,25 @@
 import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
+import SideNav from "../SideNav/SideNav";
 
 import { skills } from "../../utils/constants";
+import { useState } from "react";
 
 const App = () => {
+  const [location, setLocation] = useState("home");
+
+  const handleIdClick = (e) => {
+    setLocation(e.target.closest(".sidenav__item").id);
+  };
+
   return (
     <div className="app">
       <div className="app__content">
         <Header />
         <Main skills={skills} />
       </div>
+      <SideNav handleIdClick={handleIdClick} location={location} />
     </div>
   );
 };
--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -1,7 +1,82 @@
 import "./SideNav.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faUser,
+  faCode,
+  faGears,
+  faFeather,
+} from "@fortawesome/free-solid-svg-icons";
 
-const SideNav = () => {
-  return <h1>SideNav</h1>;
+const SideNav = ({ handleIdClick, location }) => {
+  console.log(location);
+  return (
+    <nav className="sidenav">
+      {/* sidenav__button sidenav__(location) */}
+      <ul className="sidenav__list">
+        <li className="sidenav__item" id="home" onClick={handleIdClick}>
+          <button
+            className={`sidenav__button ${
+              location === "home"
+                ? "sidenav__home_active"
+                : "sidenav__home_inactive"
+            }`}
+            name="home"
+          >
+            <FontAwesomeIcon className="sidenav__icon" icon={faHouse} />
+          </button>
+        </li>
+        <li className="sidenav__item" id="about" onClick={handleIdClick}>
+          <button
+            className={`sidenav__button ${
+              location === "about"
+                ? "sidenav__about_active"
+                : "sidenav__about_inactive"
+            }`}
+            name="about"
+          >
+            <FontAwesomeIcon className="sidenav__icon" icon={faUser} />
+          </button>
+        </li>
+        <li className="sidenav__item" id="skills" onClick={handleIdClick}>
+          <button
+            className={`sidenav__button ${
+              location === "skills"
+                ? "sidenav__skills_active"
+                : "sidenav__skills_inactive"
+            }`}
+            name="skills"
+          >
+            <FontAwesomeIcon className="sidenav__icon" icon={faCode} />
+          </button>
+        </li>
+        <li className="sidenav__item" id="works" onClick={handleIdClick}>
+          <button
+            className={`sidenav__button ${
+              location === "works"
+                ? "sidenav__works_active"
+                : "sidenav__works_inactive"
+            }`}
+            name="works"
+          >
+            <FontAwesomeIcon className="sidenav__icon" icon={faGears} />
+          </button>
+        </li>
+        <li className="sidenav__item" id="contact" onClick={handleIdClick}>
+          <button
+            className={`sidenav__button ${
+              location === "contact"
+                ? "sidenav__contact_active"
+                : "sidenav__contact_inactive"
+            }`}
+            name="contact"
+          >
+            <FontAwesomeIcon className="sidenav__icon" icon={faFeather} />
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default SideNav;
